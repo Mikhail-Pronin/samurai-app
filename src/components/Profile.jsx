eaeeb403dea0a4eca87f5437c72ac704de472ba4
Mikhail <pronin4work@gmail.com>
2020-08-05 15:10:54 +0300

    first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./Profile.css"
+const Profile = () => {
+  return <div className="content">
+    <img className="img1" src="https://www.treehugger.com/thmb/J8LNt43_b6eoLmxzI2vxmb2n_H4=/1000x701/filters:fill(auto,1)/__opt__aboutcom__coeus__resources__content_migration__mnn__images__2018__12__PurpleSkyPalmTrees1000px-c4950ffafcf942e2961bcd0b7bd1853b.jpg"></img>
+    <div>
+      ava + description
+   </div>
+    <div>
+      My posts
+    <div>
+        New post
+     </div>
+      <div className="Posts">
+        <div className="item">Post 1</div>
+        <div className="item">Post 2</div>
+      </div>
+    </div>
+  </div>
+}
+
+export default Profile;
